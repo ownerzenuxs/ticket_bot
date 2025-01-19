e179a5f58a4ba195933e1ecc41b141d4cc6cc705
ownerzenuxs <iwantvpspls@gmail.com>
2025-01-19 11:57:49 +0530

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -5,20 +5,16 @@ function big_blue_text() {
     echo -e "\e[1;34m$1\e[0m"
 }
 
-# Display "INSTALLING DEPENDENCIES" in blue and install git
-big_blue_text "INSTALLING DEPENDENCIES"
-sudo apt install git -y
-
-# Display "INSTALLING NPM" in blue and install discord.js
-big_blue_text "INSTALLING NPM"
-npm install discord.js
-
-# Display "COPYING FILES" in blue and clone the GitHub repository
-big_blue_text "COPYING FILES"
-git clone https://github.com/ownerzenuxs/TicketBot.git
+# Display "CLONING REPOSITORY" in blue and clone the GitHub repository
+big_blue_text "CLONING REPOSITORY"
+git clone https://github.com/ownerzenuxs/ticket_bot.git
 
 # Change directory to the cloned TicketBot folder
-cd TicketBot
+cd ticket_bot
 
-# Display "USE COMMAND node bot.js TO RUN BOT" in blue
-big_blue_text "USE COMMAND node bot.js TO RUN BOT"
+# Display "INSTALLING NPM DEPENDENCIES" in blue and install discord.js
+big_blue_text "INSTALLING NPM DEPENDENCIES"
+npm install discord.js
+
+# Display completion message
+big_blue_text "DEPENDENCIES INSTALLED. READY TO RUN YOUR BOT!"
